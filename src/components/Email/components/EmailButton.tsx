2a6a708b23e5027f662bfc5e5c6c81db30a856c3
machineke <markarapsoi72@gmail.com>
2026-05-08 18:18:00 +0300

    add template exports

--- src/components/Email/components/EmailButton.tsx
+++ src/components/Email/components/EmailButton.tsx
@@ -4,7 +4,7 @@ import { Button } from '@react-email/components';
 export const EmailButton = ({ children, href }: { children: React.ReactNode; href: string }) => (
   <Button 
     href={href}
-    className="bg-blue-600 rounded text-white text-[15px] font-semibold no-underline text-center px-5 py-3 w-full block"
+    className="bg-blue-600 rounded text-white text-[15px] font-semibold no-underline text-center px-6 py-3 inline-block"
   >
     {children}
   </Button>
