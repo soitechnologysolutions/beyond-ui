64fe9ecd8a338d4749b654ac0a16eddb53ca7a25
machineke <markarapsoi72@gmail.com>
2026-05-08 20:37:34 +0300

    add email componenents templates

--- src/components/Email/templates/marketing/index.ts
+++
@@ -1,7 +0,0 @@
-export * from './newsletters';
-export * from './lead-nurturing';
-export * from './promotional';
-export * from './events';
-export * from './reengagement';
-export * from './social';
-export * from './surveys';
